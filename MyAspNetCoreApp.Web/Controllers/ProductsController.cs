03f6e4c65d6e50c362cb5eeaa12fcd9282a78e0a
[TugcenB] <ttbilgin1@gmail.com>
2023-01-17 15:12:59 +0300

    Saving data to Product table

--- MyAspNetCoreApp.Web/Controllers/ProductsController.cs
+++ MyAspNetCoreApp.Web/Controllers/ProductsController.cs
@@ -5,19 +5,31 @@ namespace MyAspNetCoreApp.Web.Controllers
 {
     public class ProductsController : Controller
     {
+
+        private AppDbContext _context;
         private readonly ProductRepository _productRepository;
 
-        public ProductsController()
+        public ProductsController(AppDbContext context)
         {
             _productRepository = new ProductRepository();
 
-           
+           _context= context;
+
+            if(!_context.Products.Any())
+            {
+                _context.Products.Add(new Product() { Name = "Kalem 1", Stock = 10, Price = 100 });
+                _context.Products.Add(new Product() { Name = "Kalem 2", Stock = 10, Price = 200 });
+                _context.Products.Add(new Product() { Name = "Kalem 3", Stock = 10, Price = 300 });
+
+                _context.SaveChanges();
+            }
             
+
         }
 
         public IActionResult Index()
         {
-            var products = _productRepository.GetAll();
+            var products = _context.Products.ToList();
 
             return View(products);
         }
