54bb9e00a178b812ce5e73f6fdd828f6ff450702
[TugcenB] <ttbilgin1@gmail.com>
2023-01-31 13:52:30 +0300

    Form Elements Datetime

--- MyAspNetCoreApp.Web/Models/Product.cs
+++ MyAspNetCoreApp.Web/Models/Product.cs
@@ -8,6 +8,7 @@
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public string Description { get; set; }
+        public DateTime? PublishDate { get; set; }
         public string? Color { get; set; }
         public bool IsPublish { get; set; }
         public int Expire { get; set; }
