77825fd231e539a0f5f77df8f73ea5f7781f0bd6
[TugcenB] <ttbilgin1@gmail.com>
2023-02-14 21:10:46 +0300

    Model/Form Validations: Required-3

--- MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
+++ MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
@@ -13,9 +13,12 @@ namespace MyAspNetCoreApp.Web.ViewModels
         [Required(ErrorMessage = "Stok alanı boş olamaz")]
         public int? Stock { get; set; }
         public string Description { get; set; }
+        [Required(ErrorMessage = "PublishDate alanı boş olamaz")]
         public DateTime? PublishDate { get; set; }
+        [Required(ErrorMessage = "Color alanı boş olamaz")]
         public string? Color { get; set; }
         public bool IsPublish { get; set; }
-        public int Expire { get; set; }
+        [Required(ErrorMessage = "Expire alanı boş olamaz")]
+        public int? Expire { get; set; }
     }
 }
