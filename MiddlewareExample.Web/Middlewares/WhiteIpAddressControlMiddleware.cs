b6981c2a6a81fdb351c956e0b2a5e712f2a557ab
[TugcenB] <ttbilgin1@gmail.com>
2023-03-06 11:37:39 +0300

    Middleware: Sample Scenario-2

--- MiddlewareExample.Web/Middlewares/WhiteIpAddressControlMiddleware.cs
+++ MiddlewareExample.Web/Middlewares/WhiteIpAddressControlMiddleware.cs
@@ -1,19 +1,35 @@
-﻿namespace MiddlewareExample.Web.Middlewares
+﻿using System.Net;
+
+namespace MiddlewareExample.Web.Middlewares
 {
     public class WhiteIpAddressControlMiddleware
     {
         private readonly RequestDelegate _requestDelegate;
-
+        private const string WhiteIpAddress = "::1";
         public WhiteIpAddressControlMiddleware(RequestDelegate requestDelegate)
         {
             _requestDelegate = requestDelegate;
         }
 
-        public async Task Invoke Aysnc(HttpContent context)
+        public async Task InvokeAysnc(HttpContext context)
         {
             //IPV4 => 127.0.0.1 => localhost
             //       IP address     DNS name
             //IPV6 => ::1 => localhost
+            var reqIpAddress = context.Connection.RemoteIpAddress;
+            bool anyWhiteIpAddress = IPAddress.Parse(WhiteIpAddress).Equals(reqIpAddress);
+
+            if(anyWhiteIpAddress==true)
+            {
+                await _requestDelegate(context);
+            }
+            else
+            {
+                context.Response.StatusCode = HttpStatusCode.Forbidden.GetHashCode();
+
+                await context.Response.WriteAsync("Forbidden");
+            }
+
          }
     }
 }
