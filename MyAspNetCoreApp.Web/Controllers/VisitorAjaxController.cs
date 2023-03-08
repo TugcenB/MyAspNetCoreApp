a41a8c828a67b5a2365e3480e5dd33f6ad65f26c
[TugcenB] <ttbilgin1@gmail.com>
2023-03-08 15:05:48 +0300

    Ajax Action Method: VisitorAjaxController

---
+++ MyAspNetCoreApp.Web/Controllers/VisitorAjaxController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyAspNetCoreApp.Web.Models;
+using MyAspNetCoreApp.Web.ViewModels;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class VisitorAjaxController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly AppDbContext _context;
+
+        public VisitorAjaxController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveVisitorComment(VisitorViewModel visitorViewModel)
+        {
+                var visitor = _mapper.Map<Visitor>(visitorViewModel);
+
+                visitor.Created = DateTime.Now;
+                _context.Visitors.Add(visitor);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(HomeController.Visitor));
+
+
+        }
+
+        [HttpGet]
+        public IActionResult VisitorCommentList()
+        {
+            var visitors = _context.Visitors.ToList();
+
+            var visitorViewModels = _mapper.Map<List<VisitorViewModel>>(visitors);
+
+            return Json(visitorViewModels);
+        }
+
+    }
+}
